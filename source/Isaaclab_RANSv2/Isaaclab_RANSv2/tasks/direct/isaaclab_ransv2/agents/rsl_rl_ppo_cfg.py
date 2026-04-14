01f5adbd7371b2e2133f10499101144e4d005dc9
ricard-inho <ricardmarsalicastan@gmail.com>
2026-04-14 19:10:59 +0200

    AutoEnvGen_PPO_Pingu_GoToPose_All_Actuators

--- source/Isaaclab_RANSv2/Isaaclab_RANSv2/tasks/direct/isaaclab_ransv2/agents/rsl_rl_ppo_cfg.py
+++ source/Isaaclab_RANSv2/Isaaclab_RANSv2/tasks/direct/isaaclab_ransv2/agents/rsl_rl_ppo_cfg.py
@@ -13,11 +13,11 @@ class PPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 16
     max_iterations = 1000
     save_interval = 500
-    experiment_name = "AutoEnvGen_PPO"
+    experiment_name = "AutoEnvGen_PPO_Pingu_GoToPose_All_Actuators"
     logger = "wandb"
-    wandb_project = "AutoEnvGen_PPO"
+    wandb_project = "AutoEnvGen_PPO_Pingu_GoToPose_All_Actuators"
     wandb_kwargs = {
-        "project": "AutoEnvGen_PPO",
+        "project": "AutoEnvGen_PPO_Pingu_GoToPose_All_Actuators",
         "entity": "spacer-rl",
         "group": "zeroG",
     }
